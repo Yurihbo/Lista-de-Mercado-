6436fbe5cceb62adc8938bf657b6be0e759aec83
Yuri de Sousa Silva <157448649+Yurihbo@users.noreply.github.com>
2026-03-22 19:09:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lista-mercado-v1";
+const CACHE_NAME = "lista-mercado-v2"; // 🔥 muda versão sempre que atualizar
 
 const urlsToCache = [
   "./",
@@ -8,16 +8,41 @@ const urlsToCache = [
   "./images/corredores.png"
 ];
 
+// Instala
 self.addEventListener("install", event => {
+  self.skipWaiting(); // força atualizar
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Ativa
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // limpa cache antigo
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch (network first 🔥)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request))
   );
 });
